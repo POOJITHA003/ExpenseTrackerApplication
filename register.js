20833939d47420e1fcb1f65ea0fa1e4ce3b1f42e
POOJITHA003 <156311535+POOJITHA003@users.noreply.github.com>
2024-04-15 12:54:10 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,69 @@
+function solve() { 
+
+    let password =  
+
+        document.getElementById('password').value; 
+
+    let repassword =  
+
+        document.getElementById('repassword').value; 
+
+   
+    let mail = 
+
+        document.getElementById('email').value; 
+
+    let flag = 1; 
+
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+  
+
+    if (!emailRegex.test(mail)) { 
+
+        flag = 0; 
+
+        pass.innerText =  
+
+            'Please enter a valid email address.'; 
+
+        setTimeout(() => { 
+
+            pass.innerText = ""; 
+
+        }, 3000); 
+
+    } 
+
+  
+
+    if (password !== repassword) { 
+
+        flag = 0; 
+
+        pass.innerText = 
+
+            "Passwords do not match. Please re-enter."; 
+
+        setTimeout(() => { 
+
+            pass.innerText = ""; 
+
+        }, 3000); 
+
+    } 
+
+  
+
+    let passwordRegex =  
+
+        /^(?=.\d)(?=.[a-zA-Z])(?=.*[^a-zA-Z0-9])\S{8,}$/; 
+
+  
+
+    
+
+    if (flag) 
+
+        alert("Form submitted"); 
+}
